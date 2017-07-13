5d31e8fefbff53cd0de59a9e4a7aa33cd069b4af
Victor Lavaud <victor.lavaud@gmail.com>
2017-07-13 17:53:44 +0100

    Added vim

---
+++ modules/qdii/manifest/init.pp
@@ -0,0 +1,12 @@
+class vim {
+  package { 'vim':
+    ensure => 'latest',
+    name => 'vim',
+  }
+  file { 'vimrc':
+    ensure => file,
+    path => '/home/qdii/.vimrc',
+    source => 'puppet://modules/qdii/dotfiles/vimrc',
+    mode => '0600',
+  }
+}
