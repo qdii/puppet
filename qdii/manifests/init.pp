d315c55120532dda89ee1cb48cf4e5c8029f4e08
Victor Lavaud <victor.lavaud@gmail.com>
2017-07-13 22:06:35 +0100

    Vim is now a submodule

--- qdii/manifests/init.pp
+++ qdii/manifests/init.pp
@@ -1,12 +1,3 @@
 class qdii {
-  package { 'vim':
-    ensure => 'latest',
-    name => 'vim',
-  }
-  file { 'vimrc':
-    ensure => file,
-    path => '/home/qdii/.vimrc',
-    source => 'puppet:///modules/qdii/dotfiles/vimrc',
-    mode => '0600',
-  }
+  include qdii::vim
 }
