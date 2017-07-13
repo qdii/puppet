fd31dea16b606050e9cfcdec2c62ba5115f145a4
Victor Lavaud <victor.lavaud@gmail.com>
2017-07-13 22:10:10 +0100

    Added i3

---
+++ qdii/manifests/i3.pp
@@ -0,0 +1,22 @@
+class qdii::i3 {
+  package { 'i3':
+    ensure => 'latest',
+    name => 'i3-wm',
+  }
+  package { 'i3blocks':
+    ensure => 'latest',
+    name => 'i3blocks',
+    require => Package['i3'],
+  }
+  package { 'i3lock':
+    ensure => 'latest',
+    name => 'i3lock',
+    require => Package['i3'],
+  }
+  file { 'i3config':
+    ensure => file,
+    path => '/home/qdii/.i3/config',
+    source => 'puppet:///modules/qdii/dotfiles/i3config',
+    mode => '0600',
+  }
+}
--- qdii/manifests/init.pp
+++ qdii/manifests/init.pp
@@ -1,3 +1,4 @@
 class qdii {
+  include qdii::i3
   include qdii::vim
 }
