36bf43229aa3ff86573a61a42fecd14ba15e9cd4
Victor Lavaud <victor.lavaud@gmail.com>
2017-07-22 00:36:44 +0100

    create group 'qdii' before user 'qdii'

--- qdii/manifests/user.pp
+++ qdii/manifests/user.pp
@@ -1,8 +1,18 @@
 class qdii::user {
+  group { 'qdii':
+    ensure => 'present',
+  }
+  group { 'wheel':
+    ensure => 'present'
+  }
   @user { 'me':
     ensure => 'present',
     name => 'qdii',
     groups => ['qdii', 'wheel'],
     membership => minimum,
+    require => [
+      Group['qdii'],
+      Group['wheel'],
+    ],
   }
 }
