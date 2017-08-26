29c178c9e98691912d2ae9b33638de9aec92f6af
Victor Lavaud <victor.lavaud@gmail.com>
2017-08-26 11:22:10 -0700

    updated muttrc

--- qdii/manifests/mutt.pp
+++ qdii/manifests/mutt.pp
@@ -10,4 +10,10 @@ class qdii::mutt {
     source => 'puppet:///modules/qdii/dotfiles/muttrc',
     mode => '0600',
   }
+  file { 'mutt-solarized':
+    ensure => file,
+    path => '/home/qdii/.mutt/mutt-colors-solarized-dark-256.muttrc',
+    source => 'puppet:///modules/qdii/dotfiles/misc/mutt-colors-solarized-dark-256.muttrc',
+    mode => '0644',
+  }
 }
