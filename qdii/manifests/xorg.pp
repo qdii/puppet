a53aad16907db4fd73c842447f3a6d90074a28a5
Victor Lavaud <victor.lavaud@gmail.com>
2018-03-27 00:07:28 +0100

    refactored xorg

--- qdii/manifests/xorg.pp
+++ qdii/manifests/xorg.pp
@@ -1,30 +1,24 @@
 class qdii::xorg {
+  File {
+    ensure => file,
+    owner  => 'qdii',
+    group  => 'qdii',
+    mode   => '0600',
+  }
   package { 'xorg-server':
     ensure => 'latest',
-    name => 'xorg-server',
+    name   => 'xorg-server',
   }
   file { 'xinitrc':
-    ensure  => file,
-    path    => '/home/qdii/.xinitrc',
-    source  => 'puppet:///modules/qdii/dotfiles/xinitrc',
-    owner   => 'qdii',
-    group   => 'qdii',
-    mode    => '0600',
+    path   => '/home/qdii/.xinitrc',
+    source => 'puppet:///modules/qdii/dotfiles/xinitrc',
   }
   file { 'Xresources':
-    ensure  => file,
-    path    => '/home/qdii/.Xresources',
-    source  => 'puppet:///modules/qdii/dotfiles/Xresources',
-    owner   => 'qdii',
-    group   => 'qdii',
-    mode    => '0600',
+    path   => '/home/qdii/.Xresources',
+    source => 'puppet:///modules/qdii/dotfiles/Xresources',
   }
   file { 'Xresources.dark':
-    ensure  => file,
-    path    => '/home/qdii/.Xresources.dark',
-    source  => 'puppet:///modules/qdii/dotfiles/Xresources.dark',
-    owner   => 'qdii',
-    group   => 'qdii',
-    mode    => '0600',
+    path   => '/home/qdii/.Xresources.dark',
+    source => 'puppet:///modules/qdii/dotfiles/Xresources.dark',
   }
 }
