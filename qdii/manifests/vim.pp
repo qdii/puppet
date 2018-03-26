9e73a7a6b95c0655a207fcdc5f4f98e75e2fda03
Victor Lavaud <victor.lavaud@gmail.com>
2018-03-27 00:10:17 +0100

    refactored vim

--- qdii/manifests/vim.pp
+++ qdii/manifests/vim.pp
@@ -1,13 +1,17 @@
 class qdii::vim {
+  File {
+    ensure => file,
+    owner  => 'qdii',
+    group  => 'qdii',
+    mode   => '0600',
+  }
   package { 'vim':
     ensure => 'latest',
     name => 'vim',
   }
   User <| title == me |> ->
   file { 'vimrc':
-    ensure => file,
     path => '/home/qdii/.vimrc',
     source => 'puppet:///modules/qdii/dotfiles/vimrc',
-    mode => '0600',
   }
 }
