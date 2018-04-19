ea4dd4c8014a728f0aa570ba3a9f40e728f49e87
Victor Lavaud <victor.lavaud@gmail.com>
2018-04-19 06:53:03 +0100

    fixed incorrect parameter name in file custom

--- qdii/manifests/arch.pp
+++ qdii/manifests/arch.pp
@@ -2,13 +2,9 @@ class qdii::arch {
   file { 'custom':
     ensure  => file,
     path    => '/etc/pacman.d/custom',
-    sources => 'puppet://modules/qdii/dotfiles/custom',
+    source  => 'puppet://modules/qdii/dotfiles/custom',
     owner   => 'root',
     group   => 'root',
     mode    => '0600',
-  }
-  file_line { 'pacman_custom':
-    ensure => present,
-    line   => 'Include = /etc/pacman.d/custom',
   }
 }
