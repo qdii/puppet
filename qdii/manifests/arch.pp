bc09602ba3b294656d606e8986a1b6d89170dd59
Victor Lavaud <victor.lavaud@gmail.com>
2019-12-15 21:39:32 +0530

    Only apply arch.pp to arch linux

--- qdii/manifests/arch.pp
+++ qdii/manifests/arch.pp
@@ -1,40 +1,41 @@
 class qdii::arch {
-  User <| title == 'me' |> {
-    require =>  Package['docker'],
-    groups +> 'docker',
-  }
-  file { 'custom':
-    ensure  => file,
-    path    => '/etc/pacman.d/custom',
-    source  => 'puppet:///modules/qdii/dotfiles/custom',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-  }
-  file { 'pacman.conf':
-    ensure  => file,
-    path    => '/etc/pacman.d/pacman.conf',
-    source  => 'puppet:///modules/qdii/dotfiles/pacman.conf',
-    owner   => 'root',
-    group   => 'root',
-    mode    => '0644',
-  }
-  file { 'custom_directory':
-    ensure => 'directory',
-    path   => '/var/cache/pacman/custom',
-    owner  => 'qdii',
-  }
-  package { 'repoctl':
-    ensure          => installed,
-    source          => 'https://github.com/cassava/repoctl/releases/download/v0.18/repoctl-0.18.tar.gz',
-    provider        => 'pacman',
-  }
-  exec { 'create-custom-repo':
-    command => '/usr/bin/repo-add /var/cache/pacman/custom/custom.db.tar',
-    require =>  [
-      File['pacman.conf', 'custom', 'custom_directory'],
-      Package['repoctl'],
-    ]
+  if $::operatingsystem == 'Archlinux' {
+    User <| title == 'me' |> {
+      require =>  Package['docker'],
+      groups +> 'docker',
+    }
+    file { 'custom':
+      ensure  => file,
+      path    => '/etc/pacman.d/custom',
+      source  => 'puppet:///modules/qdii/dotfiles/custom',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
+    file { 'pacman.conf':
+      ensure  => file,
+      path    => '/etc/pacman.d/pacman.conf',
+      source  => 'puppet:///modules/qdii/dotfiles/pacman.conf',
+      owner   => 'root',
+      group   => 'root',
+      mode    => '0644',
+    }
+    file { 'custom_directory':
+      ensure => 'directory',
+      path   => '/var/cache/pacman/custom',
+      owner  => 'qdii',
+    }
+    package { 'repoctl':
+      ensure          => installed,
+      source          => 'https://github.com/cassava/repoctl/releases/download/v0.18/repoctl-0.18.tar.gz',
+      provider        => 'pacman',
+    }
+    exec { 'create-custom-repo':
+      command => '/usr/bin/repo-add /var/cache/pacman/custom/custom.db.tar',
+      require =>  [
+        File['pacman.conf', 'custom', 'custom_directory'],
+        Package['repoctl'],
+      ]
+    }
   }
 }
-
