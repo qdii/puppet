a3469b25019ed570a563205bd00346a822fc216f
Victor Lavaud <victor.lavaud@gmail.com>
2019-12-29 20:56:53 +0100

    added correct owner and group for tmux config file

--- qdii/manifests/tmux.pp
+++ qdii/manifests/tmux.pp
@@ -6,10 +6,14 @@ class qdii::tmux {
     ensure => file,
     source => 'puppet:///modules/qdii/dotfiles/tmux',
     path   => '/home/qdii/.tmux.conf',
+    owner  => 'qdii',
+    group  => 'qdii',
   }
   -> file { 'tmux_conf_root':
     ensure => file,
     source => 'puppet:///modules/qdii/dotfiles/tmux',
     path   => '/root/.tmux.conf',
+    owner  => 'root',
+    group  => 'root',
   }
 }
