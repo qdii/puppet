3d33846d4261e5072651134221c98c7e029200eb
Victor Lavaud <victor.lavaud@gmail.com>
2019-12-30 12:29:18 +0100

    fixed compton home directory

--- qdii/manifests/picom.pp
+++ qdii/manifests/picom.pp
@@ -1,4 +1,14 @@
 class qdii::picom {
+  if $::fqdn =~ /.*\.roam\.corp\.google\.com/ {
+    $homedir = '/home/qdii'
+  }
+  elsif $::fqdn =~ /^.*\.corp\.google\.com$/ {
+    $homedir = '/usr/local/google/home/qdii'
+  }
+  else {
+    $homedir = '/home/qdii'
+  }
+
   $pkg = $::operatingsystem ? {
     'Archlinux' => 'picom',
     'Ubuntu'    => 'compton',
@@ -8,18 +18,19 @@ class qdii::picom {
     ensure => 'latest',
   }
   User <| title == me |>
-  -> file { "/home/qdii/.config":
+  -> file { "config_dir":
+    path   => "${homedir}/.config",
     ensure => 'directory',
     mode   => '0600',
   }
   -> file { 'picom_config_directory':
     ensure => 'directory',
-    path   => "/home/qdii/.config/$pkg"
+    path   => "${homedir}/.config/$pkg",
     mode   => '0600',
   }
   file { 'picom.conf':
     ensure  => file,
-    path    => "/home/qdii/.config/$pkg/$pkg.conf",
+    path    => "${homedir}/.config/$pkg/$pkg.conf",
     source  => 'puppet:///modules/qdii/dotfiles/picom.conf',
     mode    => '0600',
     require =>  File['picom_config_directory'],
