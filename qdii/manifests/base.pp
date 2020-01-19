6ca74bd4904196e0207bbeb382ec2e5e5e341837
Victor Lavaud <victor.lavaud@gmail.com>
2020-01-19 20:31:03 +0100

    install fcitx on desktop

--- qdii/manifests/base.pp
+++ qdii/manifests/base.pp
@@ -35,6 +35,8 @@ class qdii::base {
     #        'acpi',
     #        'audacity',
     #        'fcitx',
+    #        'fcitx-libpinyin',
+    #        'fcitx-im',
     #        'firefox',
     #        'gimp',
     #        'nextcloud-client',
