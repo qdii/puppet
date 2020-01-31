e14e73d73bc1fccda86e191b9b29d2e2071d8a33
Victor Lavaud <victor.lavaud@gmail.com>
2020-01-31 22:46:05 +0100

    pscircles

--- qdii/manifests/i3.pp
+++ qdii/manifests/i3.pp
@@ -78,4 +78,12 @@ class qdii::i3 {
     group => 'qdii',
     mode => '0777',
   }
+  file { 'pscircles_bg':
+    ensure => file,
+    path   => '/usr/local/bin/pscircles_bg',
+    source => 'puppet:///modules/qdii/misc/pscircles_bg',
+    owner  => 'qdii',
+    group  => 'qdii',
+    mode   => '0777',
+  }
 }
