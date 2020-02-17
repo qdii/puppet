761eec4798bfab4005b50c7ad4a9d0b22fa294b8
Victor Lavaud <victor.lavaud@gmail.com>
2020-02-17 22:52:52 +0100

    fixed missing irssi directory

--- qdii/manifests/irssi.pp
+++ qdii/manifests/irssi.pp
@@ -9,14 +9,20 @@ class qdii::irssi {
     ensure => 'latest',
     name => 'irssi',
   }
+  file { 'irssi-directory':
+    ensure => 'directory',
+    path   => '/home/qdii/.irssi',
+  }
   file { 'irssi-config':
-    ensure =>  file,
-    path   => '/home/qdii/.irssi/config',
-    source => 'puppet:///modules/qdii/dotfiles/irssi-config',
+    ensure  =>  file,
+    path    => '/home/qdii/.irssi/config',
+    source  => 'puppet:///modules/qdii/dotfiles/irssi-config',
+    require => File['irssi-directory'],
   }
   file { 'irssi-solarized-theme':
     ensure =>  file,
     path   => '/home/qdii/.irssi/solarized-universal.theme',
     source => 'puppet:///modules/qdii/misc/solarized-universal.theme',
+    require => File['irssi-directory'],
   }
 }
