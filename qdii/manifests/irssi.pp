f53902c1e7594917f22e12009daaf9f5debb1372
Victor Lavaud <qdii@google.com>
2020-04-02 11:29:59 +0200

    mitigate breakage with irssi for home

--- qdii/manifests/irssi.pp
+++ qdii/manifests/irssi.pp
@@ -9,20 +9,25 @@ class qdii::irssi {
     ensure => 'latest',
     name => 'irssi',
   }
-  file { 'irssi-directory':
-    ensure => 'directory',
-    path   => '/home/qdii/.irssi',
+
+  if $::fqdn =~ /.*\.roam\.corp\.google\.com/ {
   }
-  file { 'irssi-config':
-    ensure  =>  file,
-    path    => '/home/qdii/.irssi/config',
-    source  => 'puppet:///modules/qdii/dotfiles/irssi-config',
-    require => File['irssi-directory'],
-  }
-  file { 'irssi-solarized-theme':
-    ensure =>  file,
-    path   => '/home/qdii/.irssi/solarized-universal.theme',
-    source => 'puppet:///modules/qdii/misc/solarized-universal.theme',
-    require => File['irssi-directory'],
+  else {
+    file { 'irssi-directory':
+      ensure => 'directory',
+      path   => '/home/qdii/.irssi',
+    }
+    file { 'irssi-config':
+      ensure  =>  file,
+      path    => '/home/qdii/.irssi/config',
+      source  => 'puppet:///modules/qdii/dotfiles/irssi-config',
+      require => File['irssi-directory'],
+    }
+    file { 'irssi-solarized-theme':
+      ensure =>  file,
+      path   => '/home/qdii/.irssi/solarized-universal.theme',
+      source => 'puppet:///modules/qdii/misc/solarized-universal.theme',
+      require => File['irssi-directory'],
+    }
   }
 }
