a621125f2ad3e806483d8d9a1dd717c91c3915f8
Victor Lavaud <victor.lavaud@gmail.com>
2020-05-14 08:47:07 +0200

    Added 10-gpu.conf

--- qdii/manifests/optimus.pp
+++ qdii/manifests/optimus.pp
@@ -3,5 +3,11 @@ class qdii::optimus {
         package { 'nvidia-prime':
             ensure => latest,
         }
+        file { '10-gpu.conf':
+            source => "puppet:///modules/qdii/dotfiles/10-gpu.conf",
+            path   => "/etc/X11/xorg.conf.d/10-gpu.conf",
+            owner  => "root",
+            mode   => "0644",
+        }
     }
 }
